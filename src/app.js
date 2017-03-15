f1f4947a83e58a5f9f6cc1e061bf878c3c3b8cf0
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-03-14 17:39:18 -0700

    Got it working as a ReactJS app! Woo!

--- src/app.js
+++ src/app.js
@@ -1,14 +1,7 @@
-'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const HOURS = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
-var storeNames = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
-var storeIds = ['first-pike', 'sea-tac', 'sea-center', 'cap-hill', 'alki'];
-var minCusts = [23, 3, 11, 20, 2];
-var maxCusts = [65, 24, 38, 38, 16];
-var avgCookies = [6.3, 1.2, 3.7, 2.3, 4.6];
-
-var stores = [];
 
 function Store(name, id, minCust, maxCust, avgCookies) {
   this.name = name;
@@ -24,7 +17,7 @@ function Store(name, id, minCust, maxCust, avgCookies) {
     sales = [];
     this.total = 0;
     for (var i = 0; i < HOURS.length; i ++) {
-      hourlySale = Math.floor(calcRandomCust() * this.avgCookies)
+      let hourlySale = Math.floor(calcRandomCust() * this.avgCookies)
       sales.push(hourlySale);
       this.total += hourlySale;
     }
@@ -34,9 +27,8 @@ function Store(name, id, minCust, maxCust, avgCookies) {
     var newDiv = document.createElement('div');
     newDiv.id = this.id;
     document.body.appendChild(newDiv);
-    React.render(<NewSection sales={ sales } title={ this.name } total={ this.total }/>, newDiv); 
+    ReactDOM.render(<NewSection sales={ sales } title={ this.name } total={ this.total }/>, newDiv); 
   };
-  stores.push(this);
 };
 
 
@@ -69,8 +61,9 @@ var ListItem = React.createClass({
   }
 });
 
+export default Store;
 
-for (var i = 0; i < 5; i++) {
-  var store = new Store(storeNames[i], storeIds[i], minCusts[i], maxCusts[i], avgCookies[i]);
-  store.renderHTML();
-}
+// for (var i = 0; i < 5; i++) {
+//   var store = new Store(storeNames[i], storeIds[i], minCusts[i], maxCusts[i], avgCookies[i]);
+//   store.renderHTML();
+// }
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Store from './app';
+
+var storeNames = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
+var storeIds = ['first-pike', 'sea-tac', 'sea-center', 'cap-hill', 'alki'];
+var minCusts = [23, 3, 11, 20, 2];
+var maxCusts = [65, 24, 38, 38, 16];
+var avgCookies = [6.3, 1.2, 3.7, 2.3, 4.6];
+
+for (var i = 0; i < 5; i++) {
+  var store = new Store(storeNames[i], storeIds[i], minCusts[i], maxCusts[i], avgCookies[i]);
+  store.renderHTML();
+}
+
+// ReactDOM.render(<App />, document.body);
